6452387103aeca8581864e39b62c9f52a2b47c88
JoniAguero <aguerojonathanivan@gmail.com>
2018-07-28 23:28:10 -0300

    modifications

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import { EventBriteAPI } from "./eventbrite.js";
 import * as UI from "./interfaz.js";
 
 const api = new EventBriteAPI();
+let listCard;
 
 api.obtenerCategorias().then(data => {
     console.log(data);
@@ -24,10 +25,17 @@ UI.buscarButton.addEventListener('click', (e) => {
     const categoriaSeleccionada = UI.selectCategorias.value;
     console.log(busqueda);
     console.log(categoriaSeleccionada);
+    console.log(UI.eventosSection);
+
+    while (UI.eventosSection.firstChild) {
+        UI.eventosSection.removeChild(UI.eventosSection.firstChild);
+    }
 
     api.obtenerBusqueda(busqueda, categoriaSeleccionada).then(data => {
         
         console.log(data);
+        console.log(UI.eventosSection);
+        
         
         const eventos = data.events;
 
@@ -35,29 +43,53 @@ UI.buscarButton.addEventListener('click', (e) => {
             const card = document.createElement('div');
             card.classList.add('card');
             card.style.display = "inline-block";
+            card.style.marginRight = "10px";
+            card.style.height = "100px";
+            card.style.width = "380px";
+            card.style.transition = "0.3s";
+            card.style.margin = "20px";
+            card.style.cursor = "pointer";
             const cardImage = document.createElement('div');
             cardImage.classList.add('card-image');
             const image = document.createElement('img');
             image.src = element.logo.url;
-            const titleCard = document.createElement('span');
-            titleCard.classList.add('card-title');
-            titleCard.appendChild(document.createTextNode(element.name.text));
             cardImage.appendChild(image);
-            cardImage.appendChild(titleCard);
             const cardContent = document.createElement('div');
-            cardContent.appendChild(document.createTextNode(element.end.timezone));
+            cardContent.classList.add('card-content');
+            cardContent.appendChild(document.createTextNode(element.name.text));
             card.appendChild(cardImage);
             card.appendChild(cardContent);
-
+            
             UI.eventosSection.appendChild(card);
+           
         });
 
-        
+    })
+    .then( () => {
+        listCard = document.querySelectorAll('.card');  
+        listCard.forEach(element => {
 
+            element.addEventListener('mouseover', () => {
+                e.preventDefault();
+                
+                element.style.height = "110px";
+                element.style.width = "418px";
 
+            });
+
+            element.addEventListener('mouseout', () => {
+                e.preventDefault();
+
+                element.style.height = "100px";
+                element.style.width = "400px";
+
+            })
+            
+        });
     })
     
-})
+});
+
 
 
 // UI.selectCategorias.addEventListener('change', (e) => {
